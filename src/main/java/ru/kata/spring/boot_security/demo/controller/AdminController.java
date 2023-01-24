b20bab2feb24b36f7d21dabd80bf7aeea5d9310c
Mihail <quickautotrans@gmail.com>
2023-01-24 11:11:18 +0300

    update7

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -15,7 +15,7 @@ import ru.kata.spring.boot_security.demo.service.UserService;
 public class AdminController {
     private final UserService userService;
 
-    @Autowired
+
     public AdminController(UserService userService) {
         this.userService = userService;
     }
--- src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -58,7 +58,7 @@ public class UserService implements UserDetailsService {
    }
 
    @Override
-   @Transactional
+   @Transactional(readOnly = true)
    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
       User user = findByUsername(username);
       if (user == null) {
